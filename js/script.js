de84cacd94ec896ebef05c675d8925ee89d3e68d
Tim Kelly <timpkelly1@gmail.com>
2020-03-30 11:16:55 +0100

    Last commit to clean up whatever happened

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 // The simple way to set up the gameboard 
 
-<<<<<<< HEAD
 let gameboard = ['x','x','x',3,4,5,6,7,8]
 let outcomes = ['x', 'o']
 
@@ -17,23 +16,6 @@ function checkBoard() {
     });
 }
 
-checkBoard();
-=======
-let gameboard = [0,1,2,3,4,5,6,7,8]
-let outcomes = ['x', 'o']
-
-outcomes.forEach(xo => {
-    if (gameboard[0] == xo && gameboard[1] == xo && gameboard[2] == xo) {checkWinner(xo)}
-    else if (gameboard[3] == xo && gameboard[4] == xo && gameboard[5] == xo) {checkWinner(xo)}
-    else if (gameboard[6] == xo && gameboard[7] == xo && gameboard[8] == xo) {checkWinner(xo)}
-    else if (gameboard[0] == xo && gameboard[3] == xo && gameboard[6] == xo) {checkWinner(xo)}
-    else if (gameboard[1] == xo && gameboard[4] == xo && gameboard[7] == xo) {checkWinner(xo)}
-    else if (gameboard[2] == xo && gameboard[5] == xo && gameboard[8] == xo) {checkWinner(xo)}
-    else if (gameboard[0] == xo && gameboard[4] == xo && gameboard[8] == xo) {checkWinner(xo)}
-    else if (gameboard[2] == xo && gameboard[4] == xo && gameboard[6] == xo) {checkWinner(xo)}
-});
->>>>>>> 09b55d3e1b0b3ec3242f4f464d07f455f81884ac
-
 function checkWinner(xo) {
     if(xo == 'x') {
         alert('player 1 wins')
@@ -41,6 +23,8 @@ function checkWinner(xo) {
         alert('player 2 wins')
     }
 }
+
+checkBoard();
 
 // Creating the player objects
 
