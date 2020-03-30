9964a48d44170ee2a889a0a24ce2a37f9cd4a3e2
Tim Kelly <timpkelly1@gmail.com>
2020-03-30 14:26:19 +0100

    Cleaned up the game logic so everything works as expected, cleaned up the code comments for next steps

--- js/script.js
+++ js/script.js
@@ -16,27 +16,29 @@ for (i=0; i < 9; i++) {
     boxes.push(document.querySelector(`[data-value = "${i}"]`))
 };
 let outcomes = ['X', 'O'];
-let xo = 'X';
 let count = 0;
+let xo;
+let gameComplete;
 
 boxes.forEach((box, index) => {
     box.addEventListener('click', () => {
         gameLogic(box,index)
-        updateBoard();
-        checkBoard();
     })
 })
 
 function gameLogic(box,index) {
-    console.log(box.textContent)
-    if(box.textContent === '') {
-        if (xo == 'X') {
-            xo = 'O'
-        } else {
-            xo = 'X'
+    if (!gameComplete) {
+        if(box.textContent === '') {
+            if (xo == 'X') {
+                xo = 'O'
+            } else {
+                xo = 'X'
+            }
+            count++
+            gameboard[index] = xo
+            updateBoard();
+            checkBoard();
         }
-        count++
-        gameboard[index] = xo
     }
 }
 
@@ -47,22 +49,42 @@ function updateBoard() {
 }
 function checkBoard() {
     outcomes.forEach(xo => {
-        if (gameboard[0] == xo && gameboard[1] == xo && gameboard[2] == xo) {checkWinner(xo)}
-        else if (gameboard[3] == xo && gameboard[4] == xo && gameboard[5] == xo) {checkWinner(xo)}
-        else if (gameboard[6] == xo && gameboard[7] == xo && gameboard[8] == xo) {checkWinner(xo)}
-        else if (gameboard[0] == xo && gameboard[3] == xo && gameboard[6] == xo) {checkWinner(xo)}
-        else if (gameboard[1] == xo && gameboard[4] == xo && gameboard[7] == xo) {checkWinner(xo)}
-        else if (gameboard[2] == xo && gameboard[5] == xo && gameboard[8] == xo) {checkWinner(xo)}
-        else if (gameboard[0] == xo && gameboard[4] == xo && gameboard[8] == xo) {checkWinner(xo)}
-        else if (gameboard[2] == xo && gameboard[4] == xo && gameboard[6] == xo) {checkWinner(xo)}
+        if (gameboard[0] == xo && gameboard[1] == xo && gameboard[2] == xo) {checkWinner(xo, 0, 1, 2)}
+        else if (gameboard[3] == xo && gameboard[4] == xo && gameboard[5] == xo) {checkWinner(xo, 3, 4, 5)}
+        else if (gameboard[6] == xo && gameboard[7] == xo && gameboard[8] == xo) {checkWinner(xo, 6, 7, 8)}
+        else if (gameboard[0] == xo && gameboard[3] == xo && gameboard[6] == xo) {checkWinner(xo, 0, 3, 6)}
+        else if (gameboard[1] == xo && gameboard[4] == xo && gameboard[7] == xo) {checkWinner(xo, 1, 4, 7)}
+        else if (gameboard[2] == xo && gameboard[5] == xo && gameboard[8] == xo) {checkWinner(xo, 2, 5, 8)}
+        else if (gameboard[0] == xo && gameboard[4] == xo && gameboard[8] == xo) {checkWinner(xo, 0, 4, 8)}
+        else if (gameboard[2] == xo && gameboard[4] == xo && gameboard[6] == xo) {checkWinner(xo, 2, 4, 6)}
         else if (count > 8) {alert('It\'s a draw!')}
     });
 }
 
-function checkWinner(xo) {
+function checkWinner(xo, a, b, c) {
+    boxes[a].classList.add("win");
+    boxes[b].classList.add("win");
+    boxes[c].classList.add("win");
     if(xo == 'x') {
         alert(player1.congratulate)
     } else {
         alert(player2.congratulate)
     }
+    gameComplete = true;
 }
+
+/* 
+The things you need to create to make it all work together
+- Update player 1's name
+- Update player 2's name
+- Update title for instructions on what player's turn
+- Update title when the game is finished
+- Update the discard game icon for when game is started
+- Update the restart game icon for when game is finished
+- Create function to discard/restart the game
+- Store the player's names in localStorage
+- Clean up all the functions into factory functions and modules
+- Clean up the code
+- Get live on Github pages
+- Contribute to The Odin Project curriculum
+*/
