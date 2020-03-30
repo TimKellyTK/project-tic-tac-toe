e26a8fae84a7e64ddc6b02bb10569b2c016d61aa
Tim Kelly <timpkelly1@gmail.com>
2020-03-30 11:07:56 +0100

    Created a basic gameboard in JavaScript for the logic of the game and checkign for winners

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-alert('hello world')
+// The simple way to set up the gameboard 
+
+let gameboard = [0,1,2,3,4,5,6,7,8]
+let outcomes = ['x', 'o']
+
+outcomes.forEach(xo => {
+    if (gameboard[0] == xo && gameboard[1] == xo && gameboard[2] == xo) {checkWinner(xo)}
+    else if (gameboard[3] == xo && gameboard[4] == xo && gameboard[5] == xo) {checkWinner(xo)}
+    else if (gameboard[6] == xo && gameboard[7] == xo && gameboard[8] == xo) {checkWinner(xo)}
+    else if (gameboard[0] == xo && gameboard[3] == xo && gameboard[6] == xo) {checkWinner(xo)}
+    else if (gameboard[1] == xo && gameboard[4] == xo && gameboard[7] == xo) {checkWinner(xo)}
+    else if (gameboard[2] == xo && gameboard[5] == xo && gameboard[8] == xo) {checkWinner(xo)}
+    else if (gameboard[0] == xo && gameboard[4] == xo && gameboard[8] == xo) {checkWinner(xo)}
+    else if (gameboard[2] == xo && gameboard[4] == xo && gameboard[6] == xo) {checkWinner(xo)}
+});
+
+function checkWinner(xo) {
+    if(xo == 'x') {
+        alert('player 1 wins')
+    } else {
+        alert('player 2 wins')
+    }
+}
+
+// Creating the player objects
+
+
+
+
+// Creating the DOM layer manipulation
