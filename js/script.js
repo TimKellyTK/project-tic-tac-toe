e954a8b354b7acd6ec2b0ff89b4ecf71354fa6bc
Tim Kelly <timpkelly1@gmail.com>
2020-03-30 13:13:12 +0100

    Created a function for the game logic

--- js/script.js
+++ js/script.js
@@ -15,44 +15,36 @@ let boxes = [];
 for (i=0; i < 9; i++) {
     boxes.push(document.querySelector(`[data-value = "${i}"]`))
 };
-let outcomes = ['x', 'o'];
+let outcomes = ['X', 'O'];
+let xo = 'X';
+let count = 0;
+
+boxes.forEach((box, index) => {
+    box.addEventListener('click', () => {
+        gameLogic(box,index)
+        updateBoard();
+        checkBoard();
+    })
+})
+
+function gameLogic(box,index) {
+    console.log(box.textContent)
+    if(box.textContent === '') {
+        if (xo == 'X') {
+            xo = 'O'
+        } else {
+            xo = 'X'
+        }
+        count++
+        gameboard[index] = xo
+    }
+}
 
 function updateBoard() {
     for (i=0; i < 9; i++) {        
         boxes[i].textContent = gameboard[i]
     }
 }
-updateBoard();
-
-boxes.forEach((box, index) => {
-    box.addEventListener('click', () => {
-        gameboard[index] = 'oi'
-        updateBoard();
-    })
-}) 
-
-/* let boxes = {};
-for (i=0; i < 9; i++) {boxes[`b${i}`] = document.querySelector(`[data-value = "${i}"]`)};
-
-function updateBoard() {
-    for (i=0; i < 9; i++) {        
-        boxes[`b${i}`].textContent = gameboard[i]
-    }
-}
-updateBoard();
-
-boxes.forEach(box => {
-    box.addEventListener('click', alert('hello world'))
-}) */
-
-
-
-// it's bad code becaues it relies on this weird gameboard, it should just be a simple for loop
-
-
-
-// Add event listeners to the board to say when they click box.b0 -> gameboard.0 = 'x', change textContent and checkBoard() and changeTurn
-
 function checkBoard() {
     outcomes.forEach(xo => {
         if (gameboard[0] == xo && gameboard[1] == xo && gameboard[2] == xo) {checkWinner(xo)}
@@ -63,6 +55,7 @@ function checkBoard() {
         else if (gameboard[2] == xo && gameboard[5] == xo && gameboard[8] == xo) {checkWinner(xo)}
         else if (gameboard[0] == xo && gameboard[4] == xo && gameboard[8] == xo) {checkWinner(xo)}
         else if (gameboard[2] == xo && gameboard[4] == xo && gameboard[6] == xo) {checkWinner(xo)}
+        else if (count > 8) {alert('It\'s a draw!')}
     });
 }
 
@@ -73,7 +66,3 @@ function checkWinner(xo) {
         alert(player2.congratulate)
     }
 }
-
-checkBoard();
-
-// Creating the DOM layer manipulation
